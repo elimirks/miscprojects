d4fab77b62566356ab08af8af1c7d38bef591c78
Elijah Mirecki <elimirks@gmail.com>
2017-09-16 12:41:15 +0200

    Add board & vegatation spread dynamics
    To-do:
    - Add a water algorithm (randomly draw lakes and rivers)
    - Explain the world generation algorithm

--- mireckisgameoflife/organism.js
+++ mireckisgameoflife/organism.js
@@ -1,13 +1,14 @@
-var VEGETATION_SPREAD = 0.01;
-var TURN_SPEED_MS = 500;
-var SCALE = 2;
+var VEGETATION_SPAWN = 0.01;
+var VEGETATION_SPREAD = 0.005;
+var TURN_SPEED_MS = 100;
+var SCALE = 10;
 
 function Tile(x, y) {
     this.x = x;
     this.y = y;
 
     this.$element = $('<div/>')
-        .addclass('block')
+        .addClass('block')
         .width(SCALE)
         .height(SCALE)
         .offset({
@@ -16,13 +17,13 @@ function Tile(x, y) {
         });
 }
 
-Tile.prototype.getColor() {
+Tile.prototype.getColor = function() {
     throw "getColor must be implemented in subclasses!";
 }
 
-Tile.prototype.update(board) {
+Tile.prototype.update = function(board) {
     this.$element
-        .backgroundColor(this.getColor())
+        .css('background-color', this.getColor())
         .offset({
             top: this.y * SCALE,
             left: this.x * SCALE,
@@ -30,9 +31,35 @@ Tile.prototype.update(board) {
 }
 
 function Board($container, width, height) {
+    this.width = width;
+    this.height = height;
+    this.$container = $container;
+
+    this.tiles = [];
+    for (var y = 0; y < height; y++) {
+        var row = [];
+        for (var x = 0; x < width; x++) {
+            var tile = new Earth(x, y, Math.random() < VEGETATION_SPAWN);
+            row.push(tile);
+            $container.append(tile.$element);
+            console.log("Adding " + x + "," + y);
+        }
+        this.tiles.push(row);
+    }
+
+    this.update();
 }
 
-Board.prototype.update() {
+Board.prototype.update = function() {
+    for (var y = 0; y < this.height; y++) {
+        for (var x = 0; x < this.width; x++) {
+            this.tiles[y][x].update(this);
+        }
+    }
+}
+
+Board.prototype.spreadVegetation = function(x, y) {
+    this.tiles[y][x].hasVegetation = true;
 }
 
 function Earth(x, y, hasVegetation) {
@@ -40,23 +67,23 @@ function Earth(x, y, hasVegetation) {
     this.hasVegetation = hasVegetation;
 }
 
-Earth.prototype.getColor() {
+Earth.prototype.getColor = function() {
     return this.hasVegetation ? '#070' : '#770';
 }
 
-Earth.prototype.update(board) {
+Earth.prototype.update = function(board) {
     if (this.hasVegetation) {
         if (this.x > 0 && Math.random() < VEGETATION_SPREAD) {
-            board.spreadVegetation(this.x - 1, this.y, board);
+            board.spreadVegetation(this.x - 1, this.y);
         }
         if (this.y > 0 && Math.random() < VEGETATION_SPREAD) {
-            board.spreadVegetation(this.x, this.y - 1, board);
+            board.spreadVegetation(this.x, this.y - 1);
         }
         if (this.x < board.width - 1 && Math.random() < VEGETATION_SPREAD) {
-            board.spreadVegetation(this.x + 1, this.y, board);
+            board.spreadVegetation(this.x + 1, this.y);
         }
         if (this.y < board.height - 1 && Math.random() < VEGETATION_SPREAD) {
-            board.spreadVegetation(this.x, this.y + 1, board);
+            board.spreadVegetation(this.x, this.y + 1);
         }
     }
 
