e7c451e5c38fbce036d818f3e5ab14a6d9f9958a
Elijah Mirecki <elimirks@gmail.com>
2020-12-13 02:02:51 -0500

    Advent day selection

--- advent2020/app/Main.hs
+++ advent2020/app/Main.hs
@@ -1,9 +1,33 @@
 module Main where
 
+import System.Environment
+import System.Exit
+
+import Helper
+
 import Day01
 import Day02
 import Day03
 import Day04
 
+runners :: [IO ()]
+runners = [run01, run02, run03, run04]
+
+dieWithUsage :: IO a
+dieWithUsage = die "Usage: advent [digit]"
+
+pickRunner :: IO Int
+pickRunner = do
+  args <- getArgs
+
+  case safeRead <$> args of
+    [Just value] -> pure $ fromIntegral $ value - 1
+    _            -> dieWithUsage
+
 main :: IO ()
-main = run04
+main = do
+  num <- pickRunner
+
+  if num >= 0 && num <= length runners
+    then runners !! num
+    else dieWithUsage
