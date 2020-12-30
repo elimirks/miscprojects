bcd284f02a44b57e3d96ce2c60356b7e8c5e8810
Elijah Mirecki <elimirks@gmail.com>
2020-12-30 00:22:00 -0500

    Advent of Code day 10.2

--- advent2020/src/Day10.hs
+++ advent2020/src/Day10.hs
@@ -10,15 +10,28 @@ computeDiffs xs = uncurry (-) <$> zip xs (0:xs)
 solve1 :: [Integer] -> Integer
 solve1 input = ones * threes
   where
-    diff   = computeDiffs input
+    diff   = computeDiffs $ sort input
     ones   = countPredicate (1 ==) diff
     threes = countPredicate (3 ==) diff + 1 -- +1 for the built in adapter
 
+solve2 :: [Integer] -> Integer
+solve2 input = snd $ head $ dyn
+  where
+    sorted = sort input
+    xs     = sorted <> [3 + last sorted]
+    dyn    = foldl' nextRes [(0, 1)] xs -- DP list
+
+    nextRes :: [(Integer, Integer)] -> Integer -> [(Integer, Integer)]
+    nextRes previous new = (new, newDyn):previous
+      where 
+        satisfied = takeWhile ((3 >=) . (new -) . fst) previous
+        newDyn    = sum $ snd <$> satisfied
+
 run10 :: IO ()
 run10 = do
-  input <- sort <$> readInputIntegers "data/day10"
+  input <- readInputIntegers "data/day10"
   putStrLn "Part 10.1:"
   putStrLn $ show $ solve1 input
 
   putStrLn "Part 10.2:"
-  putStrLn $ "Target: " <> show (3 + foldl' max 0 input)
+  putStrLn $ show $ solve2 input
