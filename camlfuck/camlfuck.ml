d9cdcc79bc972c1926de29f4386b1a69730d44d5
Elijah Mirecki <elimirks@gmail.com>
2021-08-18 17:18:29 -0400

    Implement bracket

--- camlfuck/camlfuck.ml
+++ camlfuck/camlfuck.ml
@@ -12,6 +12,7 @@ type env = {
 
 let env = {
     pointer = ref 0;
+    (* As per the original BF spec, the max size was 30000 *)
     memory = Array.make 30000 0
   }
 
@@ -21,8 +22,8 @@ let stackParse str =
   let stack = ref [[]] in
   let f character =
     stack := match character with
-    | '>' -> (PointerLeft  :: (List.hd !stack)) :: List.tl !stack
-    | '<' -> (PointerRight :: (List.hd !stack)) :: List.tl !stack
+    | '<' -> (PointerLeft  :: (List.hd !stack)) :: List.tl !stack
+    | '>' -> (PointerRight :: (List.hd !stack)) :: List.tl !stack
     | '+' -> (Increment    :: (List.hd !stack)) :: List.tl !stack
     | '-' -> (Decrement    :: (List.hd !stack)) :: List.tl !stack
     | '.' -> (OutputChar   :: (List.hd !stack)) :: List.tl !stack
@@ -41,19 +42,26 @@ let stackParse str =
 
 let program = stackParse input
 
-let step env c =
+let rec step env c =
   let ptr = env.pointer
   and mem = env.memory
   in
   match c with
-  | PointerLeft  -> ptr := !ptr - 1
+  | PointerLeft  ->
+     if !ptr == 0 then
+       ()
+     else
+       ptr := !ptr - 1
   | PointerRight -> ptr := !ptr + 1
   | Increment    -> Array.set mem !ptr (Array.get mem !ptr + 1)
   | Decrement    -> Array.set mem !ptr (Array.get mem !ptr - 1)
-  | OutputChar   -> Printf.printf "%c" (Char.chr (Array.get env.memory 0))
-  (* TODO: Handle Bracket case *)
-  | _            -> ()
+  | OutputChar   -> Printf.printf "%c" (Char.chr (Array.get env.memory !ptr))
+  | Bracket body ->
+     while (Array.get env.memory !ptr) != 0 do
+       let _ = List.map (step env) body in
+       ()
+     done
 
 let () =
-  let _ = List.map (step env) program
-  in ()
+  let _ = List.map (step env) program in
+  ()
