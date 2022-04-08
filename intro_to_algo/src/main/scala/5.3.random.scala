73e360f7e69713a223f545abb082a483c777ad30
Elijah Mirecki <elimirks@gmail.com>
2022-04-08 16:57:03 -0400

    Add another (bad) permutation example

--- intro_to_algo/src/main/scala/5.3.random.scala
+++ intro_to_algo/src/main/scala/5.3.random.scala
@@ -22,12 +22,32 @@ def randomizeInPlace(nums: Array[Int]): Array[Int] = {
   result
 }
 
+/**
+  * For question 5.3-3.
+  * In the correct solution, it essentially works by picking a random element
+  * from the array to be put at index `i`, and then that index is never touch
+  * again. So it's the same as creating a new array, and randomly selecting
+  * elements to copy over in order.
+  * In the `0..n` solution, you have a _much higher chance_ of swapping earlier
+  * indexes more often than latter indexes, so I expect there to be some skew.
+  */
+def permuteWithAll(nums: Array[Int]): Array[Int] = {
+  val result = nums.clone
+  for (i <- 0 until result.length - 1) {
+    val tmp = result(i)
+    val n = r.nextInt(result.length)
+    result(i) = result(n)
+    result(n) = tmp
+  }
+  result
+}
+
 def findDist(f: Array[Int] => Array[Int]): Map[String, Int] = {
   val nums = (1 to 5).toArray
   val dist = mut.Map[String, Int]()
 
   for (_ <- 0 until 1000000) {
-    val result = randomizeInPlace(nums)
+    val result = f(nums)
     val resultStr = result.mkString(",")
     dist.put(resultStr, 1 + dist.getOrElse(resultStr, 0))
   }
@@ -37,8 +57,8 @@ def findDist(f: Array[Int] => Array[Int]): Map[String, Int] = {
   val distStd = stdDev(dist.values)
   val distMean = mean(dist.values)
 
-  println(s"Distribution distMax:   $distMax")
-  println(s"Distribution distMin:   $distMin")
+  println(s"Distribution max:   $distMax")
+  println(s"Distribution min:   $distMin")
   println(s"Standard deviation: $distStd (${distStd / distMean}% of mean)")
 
   dist.toMap
@@ -48,4 +68,8 @@ def findDist(f: Array[Int] => Array[Int]): Map[String, Int] = {
   println("Computing distribution of randomizeInPlace")
   println("==========================================")
   findDist(randomizeInPlace)
+
+  println("Computing distribution of permuteWithAll")
+  println("========================================")
+  findDist(permuteWithAll)
 }
