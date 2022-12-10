ff0d0453dfd30c5f4323d51ca0115373059ad8c0
Elijah Mirecki <elimirks@gmail.com>
2022-12-09 19:13:45 -0500

    Day 9a

--- advent_of_code/2022/src/day09.rs
+++ advent_of_code/2022/src/day09.rs
@@ -3,13 +3,6 @@ use std::collections::HashSet;
 use crate::common::*;
 use sscanf::sscanf;
 
-enum Dir {
-    Up,
-    Down,
-    Left,
-    Right,
-}
-
 pub fn day09() -> AocResult<()> {
     println!("Part 1: {}", part1()?);
     Ok(())
@@ -18,54 +11,28 @@ pub fn day09() -> AocResult<()> {
 pub fn part1() -> AocResult<usize> {
     let raw = std::fs::read_to_string("data/day09.txt")?;
     let mut visited = HashSet::new();
-
     let mut head_x: i64 = 0;
     let mut head_y: i64 = 0;
     let mut tail_x: i64 = 0;
     let mut tail_y: i64 = 0;
-
-    show(head_x, head_y, tail_x, tail_y);
-
     raw.lines().for_each(|line| {
-        let (d, amount) = sscanf!(line, "{char} {i64}").unwrap();
+        let (dir, amount) = sscanf!(line, "{char} {i64}").unwrap();
         for _ in 0..amount {
-            match d {
-                'R' => head_x += 1,
-                'L' => head_x -= 1,
-                'U' => head_y -= 1,
-                'D' => head_y += 1,
+            let (dx, dy) = match dir {
+                'R' => (1, 0),
+                'L' => (-1, 0),
+                'U' => (0, -1),
+                'D' => (0, 1),
                 _   => panic!("Invalid direction!"),
+            };
+            head_x += dx;
+            head_y += dy;
+            if i64::abs(head_x - tail_x).max(i64::abs(head_y - tail_y)) > 1 {
+                tail_y = head_y - dy;
+                tail_x = head_x - dx;
             }
-
-            if i64::abs(head_x - tail_x) > 1 {
-                tail_y = head_y;
-                if tail_x > head_x {
-                    tail_x -= 1;
-                } else {
-                    tail_x += 1;
-                }
-            } else if i64::abs(head_y - tail_y) > 1 {
-                tail_x = head_x;
-                if tail_y > head_x {
-                    tail_y -= 1;
-                } else {
-                    tail_y += 1;
-                }
-            }
-            show(head_x, head_y, tail_x, tail_y);
-
             visited.insert((tail_x, tail_y));
         }
     });
     Ok(visited.len())
-}
-
-fn show(head_x: i64, head_y: i64, tail_x: i64, tail_y: i64) {
-    let mut grid = vec![vec![' '; 10]; 10];
-    grid[5 + tail_y as usize][5 + tail_x as usize] = 'T';
-    grid[5 + head_y as usize][5 + head_x as usize] = 'H';
-    println!("------");
-    let x = grid.iter().map(|row| row.iter().collect::<String>()).collect::<Vec<_>>();
-    println!("{}", x.join("\n"));
-    println!("------");
 }
