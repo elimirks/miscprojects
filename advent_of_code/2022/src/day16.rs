e732a80472cda1aa317e8364e828732e181ffcbe
Elijah Mirecki <elimirks@gmail.com>
2022-12-17 20:12:45 -0500

    Day 16 part 2

--- advent_of_code/2022/src/day16.rs
+++ advent_of_code/2022/src/day16.rs
@@ -1,6 +1,8 @@
 use std::collections::{HashMap, HashSet, VecDeque};
 
 use crate::common::*;
+use bit_set::BitSet;
+use itertools::Itertools;
 use sscanf::sscanf;
 
 #[derive(Clone)]
@@ -12,72 +14,83 @@ struct Room {
 
 pub fn day16() -> AocResult<()> {
     let rooms = parse()?;
-    println!("Part 1: {}", part1(&rooms));
-    println!("Part 2: {}", part2(&rooms));
+    let distances = create_distance_matrix(&rooms);
+    let valve_rooms = rooms.iter().enumerate().filter_map(|(index, room)| {
+        if room.flow_rate == 0 {
+            None
+        } else {
+            Some(index)
+        }
+    }).collect::<Vec<usize>>();
+    let flow_rates = rooms.iter()
+        .map(|room| room.flow_rate)
+        .collect::<Vec<_>>();
+
+    println!("Part 1: {}", time_closure(|| part1(&distances, &valve_rooms, &flow_rates)));
+    println!("Part 2: {}", time_closure(|| part2(&distances, &valve_rooms, &flow_rates)));
     Ok(())
 }
 
-fn part1(rooms: &Vec<Room>) -> i64 {
-    let distances = create_distance_matrix(rooms);
-    let valve_rooms = rooms.iter().enumerate().filter_map(|(index, room)| {
-        if room.flow_rate == 0 {
-            None
-        } else {
-            Some(index)
-        }
-    }).collect::<Vec<usize>>();
-    let flow_rates = rooms.iter()
-        .map(|room| room.flow_rate)
-        .collect::<Vec<_>>();
-    let unvisited = valve_rooms.iter().cloned().collect::<HashSet<_>>();
-    get_max_release(0, 30, unvisited, &distances, &flow_rates)
+fn part1(
+    distances: &[Vec<i64>],
+    valve_rooms: &[usize],
+    flow_rates: &[i64],
+) -> i64 {
+    let unvisited = valve_rooms.iter().cloned().collect::<BitSet<_>>();
+    max_release(0, 30, unvisited, distances, flow_rates)
 }
 
-fn part2(rooms: &Vec<Room>) -> i64 {
-    let distances = create_distance_matrix(rooms);
-    let valve_rooms = rooms.iter().enumerate().filter_map(|(index, room)| {
-        if room.flow_rate == 0 {
-            None
-        } else {
-            Some(index)
-        }
-    }).collect::<Vec<usize>>();
-    let flow_rates = rooms.iter()
-        .map(|room| room.flow_rate)
-        .collect::<Vec<_>>();
-    let unvisited = valve_rooms.iter().cloned().collect::<HashSet<_>>();
-    get_max_release(0, 30, unvisited, &distances, &flow_rates)
+fn part2(
+    distances: &[Vec<i64>],
+    valve_rooms: &[usize],
+    flow_rates: &[i64],
+) -> i64 {
+    let time_limit = 26;
+    (1..(valve_rooms.len() / 2)).flat_map(|k| {
+        valve_rooms.iter().combinations(k).map(|my_valves| {
+            let my_unvisited = my_valves.iter()
+                .map(|&&index| index)
+                .collect::<BitSet<_>>();
+            let elephant_unvisited = valve_rooms.iter()
+                .filter(|&&room_idx| !my_unvisited.contains(room_idx))
+                .cloned()
+                .collect::<BitSet<_>>();
+
+            let my_release = 
+                max_release(0, time_limit, my_unvisited, distances, flow_rates);
+            let elephant_release =
+                max_release(0, time_limit, elephant_unvisited, distances, flow_rates);
+            my_release + elephant_release
+        })
+    }).max().expect("No rooms given")
 }
 
-fn get_max_release(
+fn max_release(
     index: usize,
     t_remaining: i64,
-    unvisited: HashSet<usize>,
+    unvisited: BitSet,
     distances: &[Vec<i64>],
     flow_rates: &[i64],
 ) -> i64 {
-    if unvisited.len() == 0 || t_remaining <= 0 {
+    if unvisited.is_empty() || t_remaining <= 0 {
         return 0;
     }
-    let mut ans = 0;
-    for &next in unvisited.iter() {
+    unvisited.iter().map(|next| {
         let t_travel = distances[index][next];
-        if t_remaining >= t_travel + 1 {
+        if t_remaining > t_travel {
             let mut next_unvisited = unvisited.clone();
-            next_unvisited.remove(&next);
-            let next_max_release = get_max_release(
+            next_unvisited.remove(next);
+            flow_rates[next] * (t_remaining - t_travel - 1) + max_release(
                 next,
                 t_remaining - t_travel - 1,
                 next_unvisited,
                 distances,
                 flow_rates,
-            );
-            let cumulative_rel =
-                flow_rates[next] * (t_remaining - t_travel - 1);
-            ans = ans.max(cumulative_rel + next_max_release);
+            )
+        } else {
+            0
         }
-    }
-    ans
+    }).max().unwrap()
 }
 
 fn create_distance_matrix(rooms: &Vec<Room>) -> Vec<Vec<i64>> {
