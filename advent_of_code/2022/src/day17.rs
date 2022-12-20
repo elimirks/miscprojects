5ff275e517667d460022b945b4e2a10a373915b2
Elijah Mirecki <elimirks@gmail.com>
2022-12-20 17:11:44 -0500

    Partial day 17 solution. But it's still pretty slow

--- advent_of_code/2022/src/day17.rs
+++ advent_of_code/2022/src/day17.rs
@@ -1,4 +1,9 @@
+use std::collections::VecDeque;
+
 use crate::common::*;
+
+const CHAMBER_WIDTH: usize = 7;
+type Chamber = VecDeque<[bool; CHAMBER_WIDTH]>;
 
 #[derive(Debug)]
 enum Movement {
@@ -7,130 +12,172 @@ enum Movement {
 }
 
 struct Rock {
-    width: usize,
-    height: usize,
     points: Vec<(usize, usize)>,
 }
 
 pub fn day17() -> AocResult<()> {
-    let data = std::fs::read_to_string("data/day17.txt")?.chars()
+    let data = parse(&std::fs::read_to_string("data/day17.txt")?);
+    println!("Part 1: {}", part1(&data));
+    println!("Part 2: {}", part2(&data));
+    Ok(())
+}
+
+fn parse(content: &str) -> Vec<Movement> {
+    content.chars()
         .filter_map(|c| match c {
             '>' => Some(Movement::Right),
             '<' => Some(Movement::Left),
             _   => None
         })
-        .collect::<Vec<_>>();
-    println!("Part 1: {}", part1(&data));
-    Ok(())
+        .collect::<Vec<_>>()
 }
 
 fn create_rocks() -> Vec<Rock> {
     vec![
         Rock {
-            width: 4,
-            height: 1,
             points: vec![(0, 0), (1, 0), (2, 0), (3, 0)]
         },
         Rock {
-            width: 3,
-            height: 3,
             points: vec![(1, 0), (0, 1), (1, 1), (2, 1), (1, 2)]
         },
         Rock {
-            width: 3,
-            height: 3,
             // "upside down" since we grow the chamber upwards
             points: vec![(0, 0), (1, 0), (2, 0), (2, 1), (2, 2)]
         },
         Rock {
-            width: 1,
-            height: 4,
-            points: vec![(0, 1), (0, 2), (0, 3), (0, 4)]
+            points: vec![(0, 0), (0, 1), (0, 2), (0, 3)]
         },
         Rock {
-            width: 2,
-            height: 2,
             points: vec![(0, 0), (1, 0), (0, 1), (1, 1)]
         },
     ]
 }
 
 fn is_at_rest(
-    chamber: &Vec<Vec<bool>>,
+    chamber: &Chamber,
     rock: &Rock,
     rock_x: usize,
     rock_y: usize,
 ) -> bool {
     rock.points.iter().map(|&(x, y)| {
         (x + rock_x, y + rock_y)
-    }).all(|(x, y)| {
-        if y == 0 {
-            true
-        } else if y > chamber.len() {
-            false
-        } else {
+    }).any(|(x, y)| {
+        if y > 0 && y < 1 + chamber.len() {
             chamber[y - 1][x]
+        } else if y == 0 {
+            true
+        } else {
+            false
         }
     })
 }
 
-fn print_chamber(chamber: &Vec<Vec<bool>>) {
-    println!("-------");
-    chamber.iter().rev().for_each(|row| {
-        println!("{}", row.iter().map(|&value| match value {
-            true  => '#',
-            false => '.',
-        }).collect::<String>());
-    });
-    println!("-------");
+fn rock_collides(
+    chamber: &Chamber,
+    rock: &Rock,
+    rock_x: usize,
+    rock_y: usize,
+) -> bool {
+    rock.points.iter().map(|&(x, y)| {
+        (x + rock_x, y + rock_y)
+    }).any(|(x, y)| {
+        x >= CHAMBER_WIDTH || (y < chamber.len() && chamber[y][x])
+    })
 }
 
-fn part1(movements: &[Movement]) -> i64 {
-    let chamber_width = 7;
+fn add_rock_to_chamber(
+    chamber: &mut Chamber,
+    rock: &Rock,
+    rock_x: usize,
+    rock_y: usize
+) {
+    for &(x, y) in rock.points.iter() {
+        let xp = x + rock_x;
+        let yp = y + rock_y;
+        while yp >= chamber.len() {
+            chamber.push_back([false; CHAMBER_WIDTH]);
+        }
+        chamber[yp][xp] = true;
+    }
+}
+
+fn is_prunable(chamber: &Chamber, x: usize, y: usize) -> bool {
+    if y < CHAMBER_WIDTH {
+        false
+    } else if x >= CHAMBER_WIDTH {
+        true
+    } else if !chamber[y][x] {
+        false
+    } else {
+        is_prunable(chamber, x + 1, y) || 
+            is_prunable(chamber, x + 1, y + 1) || 
+            is_prunable(chamber, x + 1, y - 1)
+    }
+}
+
+// Returns the number of rows pruned
+fn prune_chamber(chamber: &mut Chamber) -> usize {
+    if chamber.len() < CHAMBER_WIDTH {
+        return 0;
+    }
+    for y in (CHAMBER_WIDTH..chamber.len() - CHAMBER_WIDTH).rev() {
+        if is_prunable(chamber, 0, y) {
+            let prune_index = y - CHAMBER_WIDTH;
+            chamber.drain(..prune_index);
+            return prune_index;
+        }
+    }
+    0
+}
+
+fn solve(movements: &[Movement], rock_count: usize) -> usize {
     let mut move_index = 0;
     let rocks = create_rocks();
-    let _rock_count = 2022;
     // Lower indices are closer to the bottom
-    let mut chamber = Vec::<Vec<bool>>::new();
-    for i in 0..2 {
+    let mut chamber = VecDeque::<[bool; CHAMBER_WIDTH]>::new();
+    let mut amount_pruned = 0;
+    for i in 0..rock_count {
         let rock = &rocks[i % rocks.len()];
         let mut rock_x = 2;
-        let mut rock_y = chamber.len() + rock.height + 2;
+        let mut rock_y = chamber.len() + 3;
 
         loop {
-            println!("{rock_x},{rock_y}");
-            println!("{:?}", movements[move_index]);
-            match movements[move_index] {
-                Movement::Left => {
-                    if rock_x > 0 {
-                        rock_x -= 1;
-                    }
+            let proposed_x = match movements[move_index] {
+                Movement::Left => if rock_x > 0 {
+                    rock_x - 1
+                } else {
+                    0
                 },
-                Movement::Right => {
-                    if rock_x + rock.width < chamber_width {
-                        rock_x += 1;
-                    }
-                },
+                Movement::Right => rock_x + 1,
+            };
+            if !rock_collides(&chamber, rock, proposed_x, rock_y) {
+                rock_x = proposed_x;
             }
             move_index = (move_index + 1) % movements.len();
-            if rock_y == 0 || is_at_rest(&chamber, rock, rock_x, rock_y) {
+            if is_at_rest(&chamber, rock, rock_x, rock_y) {
                 break;
             } else {
                 rock_y -= 1;
             }
         }
-        println!("{rock_x},{rock_y}");
+        add_rock_to_chamber(&mut chamber, rock, rock_x, rock_y);
 
-        for &(x, y) in rock.points.iter() {
-            let xp = x + rock_x;
-            let yp = y + rock_y;
-            while yp >= chamber.len() {
-                chamber.push(vec![false; chamber_width]);
-            }
-            chamber[yp][xp] = true;
+        if i % 10000 == 0 {
+            let next_prune = prune_chamber(&mut chamber);
+            amount_pruned += next_prune;
+            rock_y -= next_prune;
         }
-
-        print_chamber(&chamber);
+        if i % 10000000 == 0 {
+            println!("{:.4}%", 100.0 * i as f64 / rock_count as f64);
+        }
     }
-    0
+    amount_pruned + chamber.len()
+}
+
+fn part1(movements: &[Movement]) -> usize {
+    solve(movements, 2022)
+}
+
+fn part2(movements: &[Movement]) -> usize {
+    solve(movements, 10000000 * 10)
 }
