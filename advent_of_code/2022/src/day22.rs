8bcb5160f0a32822e0356180691d603139875940
Elijah Mirecki <elimirks@gmail.com>
2022-12-22 11:41:03 -0500

    Day 22 part 1

--- advent_of_code/2022/src/day22.rs
+++ advent_of_code/2022/src/day22.rs
@@ -1,17 +1,19 @@
 use crate::common::*;
 
+#[derive(Debug, PartialEq)]
 enum Tile {
     Wall,
     Path,
     Void,
 }
 
-enum Movement {
+#[derive(Debug)] enum Movement {
     Forward(usize),
     Clockwise,
     Counterclockwise,
 }
 
+#[derive(Debug, Copy, Clone)]
 enum Direction {
     Right,
     Left,
@@ -19,10 +21,31 @@ enum Direction {
     Down,
 }
 
+impl Direction {
+    fn cw(&self) -> Direction {
+        match self {
+            Direction::Right => Direction::Down,
+            Direction::Left => Direction::Up,
+            Direction::Up => Direction::Right,
+            Direction::Down => Direction::Left,
+        }
+    }
+
+    fn ccw(&self) -> Direction {
+        match self {
+            Direction::Right => Direction::Up,
+            Direction::Left => Direction::Down,
+            Direction::Up => Direction::Left,
+            Direction::Down => Direction::Right,
+        }
+    }
+}
+
+#[derive(Debug)]
 struct Player {
     x: usize,
     y: usize,
-    direction: Direction,
+    dir: Direction,
 }
 
 pub fn day22() -> AocResult<()> {
@@ -31,8 +54,87 @@ pub fn day22() -> AocResult<()> {
     Ok(())
 }
 
-fn part1(map: &Vec<Vec<Tile>>, movements: &[Movement]) -> usize {
-    unimplemented!()
+fn part1(map: &[Vec<Tile>], movements: &[Movement]) -> usize {
+    let mut player = Player {
+        x: find_start_x(&map[0]),
+        y: 0,
+        dir: Direction::Right,
+    };
+    for movement in movements.iter() {
+        match movement {
+            Movement::Forward(amount) => move_forward(map, &mut player, *amount),
+            Movement::Clockwise => player.dir = player.dir.cw(),
+            Movement::Counterclockwise => player.dir = player.dir.ccw(),
+        }
+    }
+
+    let facing_id = match player.dir {
+        Direction::Right => 0,
+        Direction::Down  => 1,
+        Direction::Left  => 2,
+        Direction::Up    => 3,
+    };
+    1000 * (player.y + 1) + 4 * (player.x + 1) + facing_id
+}
+
+fn move_forward(map: &[Vec<Tile>], player: &mut Player, amount: usize) {
+    let (delta_x, delta_y) = match player.dir {
+        Direction::Right => (1, 0),
+        Direction::Left => (-1, 0),
+        Direction::Up => (0, -1),
+        Direction::Down => (0, 1),
+    };
+    for _ in 0..amount {
+        let x = player.x as i64 + delta_x;
+        let y = player.y as i64 + delta_y;
+
+        if is_in_bounds(map, x, y) {
+            if map[y as usize][x as usize] == Tile::Wall {
+                break;
+            }
+            player.x = x as usize;
+            player.y = y as usize;
+        } else {
+            wrap_forward(map, player);
+        }
+    }
+}
+
+// Attempts to cycle around a portion of the map
+// Used for if the player hits an edge
+fn wrap_forward(map: &[Vec<Tile>], player: &mut Player) {
+    let mut x = player.x as i64;
+    let mut y = player.y as i64;
+    match player.dir {
+        Direction::Right => while is_in_bounds(map, x - 1, y) {
+            x -= 1;
+        },
+        Direction::Left => while is_in_bounds(map, x + 1, y) {
+            x += 1;
+        },
+        Direction::Up => while is_in_bounds(map, x, y + 1) {
+            y += 1;
+        },
+        Direction::Down => while is_in_bounds(map, x, y - 1) {
+            y -= 1;
+        },
+    }
+    if map[y as usize][x as usize] == Tile::Path {
+        player.x = x as usize;
+        player.y = y as usize;
+    }
+}
+
+fn is_in_bounds(map: &[Vec<Tile>], x: i64, y: i64) -> bool {
+    if x < 0 || y < 0 || x >= map[0].len() as i64 || y >= map.len() as i64 {
+        false
+    } else {
+        map[y as usize][x as usize] != Tile::Void
+    }
+}
+
+fn find_start_x(row: &[Tile]) -> usize {
+    row.iter().position(|tile| *tile == Tile::Path).expect("No starting tile!")
 }
 
 fn parse(content: &str) -> (Vec<Vec<Tile>>, Vec<Movement>) {
@@ -57,8 +159,14 @@ fn parse(content: &str) -> (Vec<Vec<Tile>>, Vec<Movement>) {
     let mut i = 0;
     while i < movement_chars.len() {
         movements.push(match movement_chars[i] {
-            'R'  => Movement::Clockwise,
-            'L'  => Movement::Counterclockwise,
+            'R' => {
+                i += 1;
+                Movement::Clockwise
+            },
+            'L' => {
+                i += 1;
+                Movement::Counterclockwise
+            },
             c if c.is_ascii_digit() => {
                 let mut num = 0;
                 while i < movement_chars.len() && movement_chars[i].is_ascii_digit() {
@@ -68,7 +176,9 @@ fn parse(content: &str) -> (Vec<Vec<Tile>>, Vec<Movement>) {
                 }
                 Movement::Forward(num as usize)
             },
-            '\n' => break,
+            '\n' => {
+                break;
+            }
             c    => panic!("Unexpected movement char: `{c}`")
         });
     }
